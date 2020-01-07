fb9dca168cc4de50ce976ad291a71b78bb770a9b
Chris <blencowe1996@gmail.com>
2020-01-07 18:39:02 +0200

    New Spring project

---
+++ src/main/java/za/co/hilltop/NorthRowTest/NorthRowTestApplication.java
@@ -0,0 +1,13 @@
+package za.co.hilltop.NorthRowTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthRowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthRowTestApplication.class, args);
+	}
+
+}
