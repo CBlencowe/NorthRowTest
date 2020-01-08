75a5cbdb0a1dbfbe32305e67fe8f57edc6c5435c
Chris <blencowe1996@gmail.com>
2020-01-08 22:22:05 +0200

    Spring Security Added

---
+++ src/main/java/za/co/hilltop/NorthRowTest/SecurityConfig.java
@@ -0,0 +1,30 @@
+package za.co.hilltop.NorthRowTest;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//This is for the end point
+		http.httpBasic().and().authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/**").hasRole("ADMIN")
+			.antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+			.antMatchers(HttpMethod.PUT, "/**").hasRole("ADMIN")
+			.antMatchers(HttpMethod.DELETE, "/**").hasRole("ADMIN")
+			.anyRequest().authenticated().and().csrf().disable();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}pass").roles("ADMIN");
+	}
+}
